05baa6b57e0c5cd7454de8e98f27e0f8622feb5e
Gabe <gabe@databaseguy.com>
2021-08-29 04:41:15 -0400

    Move scripts into subfolder

--- gcp/startup.sh
+++ gcp/startup.sh
@@ -16,10 +16,13 @@ pip install --upgrade firebase-admin
 export HOME=/root
 git clone https://github.com/SethBorder/BottyG.git /bots/botty_g
 
+# Set ownership to runner account
+chown -R botrunner:botrunner /bots/botty_g
+
 # Set up venv
 virtualenv -p python3 /bots/botty_g/env
 source /bots/botty_g/env/bin/activate
-/bots/botty_g/env/bin/pip install -r /bots/botty_g/requirements.txt
+/bots/botty_g/env/bin/pip install -r /bots/botty_g/gcp/requirements.txt
 
 # Deploy supervisor configs
 cp /bots/botty_g/python_app.conf /etc/supervisor/conf.d/python_app.conf
